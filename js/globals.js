2d19d757a0d7a3fb32e0cdebeb4be723a3e642c0
Griffin Smith <smith@visionlink.org>
2014-06-21 12:42:54 -0400

    Style fix

--- js/globals.js
+++ js/globals.js
@@ -1,6 +1,13 @@
 define([], function(){
     var Globals = {};
-    Globals.Direction = {Up:1, Down:2, Left:3, Right:4, Stationary:5};
+    Globals.Direction = {
+        Up         : 1,
+        Down       : 2,
+        Left       : 3,
+        Right      : 4,
+        Stationary : 5
+    };
 
     return Globals;
-})
+});
+
