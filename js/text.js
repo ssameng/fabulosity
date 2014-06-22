e115c3f2a561b1ef06c645a833d29bff9404e592
Jonah Wallerstein <jwallers@gmail.com>
2014-06-22 11:34:34 -0400

    Text done

--- js/text.js
+++ js/text.js
@@ -1,29 +1,27 @@
 define([], function() {
     var textStyle = {
-        font: '20px Silkscreen',
+        font: '40px silkscreennormal',
         fill: '#fff',
         align: 'left'
     };
 
     var Text = {};
     Text.new = function(game, text, x, y, options) {
-        var public = {},
+        var public = game.phaser.add.text(x, y, text, textStyle);
             private = {};
 
-        private = game.phaser.add.text(x, y, text, textStyle);
-        private.alpha = 0;
+        public.alpha=0;
 
 
         var fadeSpeed = options.fadeSpeed || 2;
-        game.phaser.add.tween(public).to( { alpha: 1 }, fadeSpeed, Phaser.Easing.Linear.None, true,0,false);
+        game.phaser.add.tween(public).to( { alpha: 1 }, fadeSpeed*1000, Phaser.Easing.Linear.None, true,0,false);
 
-/*
         if (options.fadeOutAfter)
         {
             game.doAfter(function (){
                 game.phaser.add.tween(public).to( { alpha: 0 }, fadeSpeed*1000, Phaser.Easing.Linear.None, true);
             }, options.fadeOutAfter);
-        }(*/
+        }
         return public;
     };
 
